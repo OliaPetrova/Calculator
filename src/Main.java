1ff0f6c7fbe54547547b6427f7aa0f3f8f3f382e
OliaPetrova <skopindom8@gmail.com>
2024-08-07 14:41:59 +0300

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Ints Calc = new IntsCalculator();
+        System.out.println(Calc.sum(2, 2));
+        System.out.println(Calc.mult(10, 22));
+        System.out.println(Calc.pow(2, 10));
+    }
+}
